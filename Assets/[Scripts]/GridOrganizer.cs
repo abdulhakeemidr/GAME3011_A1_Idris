fd500ab5cd0e84461e7632ab796616fb1af142d4
abdulhakeemidr <56049093+abdulhakeemidr@users.noreply.github.com>
2022-01-29 15:24:03 -0500

    Set up Randomized Resource Slot Grid Generator

--- Assets/[Scripts]/GridOrganizer.cs
+++ Assets/[Scripts]/GridOrganizer.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridOrganizer : MonoBehaviour
 {
     [SerializeField]
     GameObject ResourceSlotPrefab;
     [SerializeField]
+    GridLayoutGroup gridLayout;
+    [SerializeField]
     Vector2Int GridDimensions = new Vector2Int(6, 6);
     void Start()
     {
+        gridLayout = GetComponent<GridLayoutGroup>();
+        gridLayout.constraintCount = GridDimensions.y;
+
+        
         int numCells = GridDimensions.x * GridDimensions.y;
 
         while (transform.childCount < numCells)
--- Assets/[Scripts]/ResourceSlotController.cs
+++ Assets/[Scripts]/ResourceSlotController.cs
@@ -1,18 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum ResourceSlotState
+{
+    ScanMode,
+    ExtractMode
+}
+
+public static class ResourceType
+{
+    public const int MaxResource = 5000;
+    public const int HalfResource = 2500;
+    public const int QuarterResource = 1250;
+    public const int EmptyResource = 0;
+    
+}
 
 public class ResourceSlotController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private int m_ResourceScore;
+    
+    ResourceSlotState slotState;
+    Image m_resourceSlotColor;
+    [SerializeField]
+    Color hiddenSlotColor = new Color(0.16f, 0, 0, 1f);
+    [SerializeField]
+    Color QuarterSlotColor = new Color();
+    [SerializeField]
+    Color HalfSlotColor = new Color();
+    [SerializeField]
+    Color MaxSlotColor = new Color();
+    [SerializeField]
+    Color m_ResourceColor = new Color();
+
     void Start()
     {
+        m_resourceSlotColor = GetComponent<Image>();
         
+        m_resourceSlotColor.color = hiddenSlotColor;
+        int[] RandomResource = new int[] 
+        {
+            ResourceType.MaxResource, 
+            ResourceType.HalfResource, 
+            ResourceType.QuarterResource, 
+            ResourceType.EmptyResource
+        };
+        m_ResourceScore = RandomResource[Random.Range(0, 4)];
+
+        SetResourceColor();
+        ToggleResourceColor();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetResourceColor()
+    {
+        switch(m_ResourceScore)
+        {
+            case ResourceType.EmptyResource:
+            m_ResourceColor = hiddenSlotColor;
+            break;
+            case ResourceType.QuarterResource:
+            m_ResourceColor = QuarterSlotColor;
+            break;
+            case ResourceType.HalfResource:
+            m_ResourceColor = HalfSlotColor;
+            break;
+            case ResourceType.MaxResource:
+            m_ResourceColor = MaxSlotColor;
+            break;
+        }
+    }
+
+    void ToggleResourceColor()
+    {
+        if(m_resourceSlotColor.color == hiddenSlotColor)
+        {
+            m_resourceSlotColor.color = m_ResourceColor;
+        }
+        else
+        {
+            m_resourceSlotColor.color = hiddenSlotColor;
+        }
+    }
+
+    void ToggleScanMode()
+    {
+
+    }
+
+    void ToggleExtractMode()
+    {
+
     }
 }
