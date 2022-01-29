9d845ba15c54b559ff6a1525b58c77a8dc8b9b93
abdulhakeemidr <56049093+abdulhakeemidr@users.noreply.github.com>
2022-01-29 11:15:46 -0500

    Fit Grid Layout to Screen

--- Assets/[Scripts]/GridOrganizer.cs
+++ Assets/[Scripts]/GridOrganizer.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class GridOrganizer : MonoBehaviour
 {
+    [SerializeField]
+    GameObject ResourceSlotPrefab;
+    [SerializeField]
+    Vector2Int GridDimensions = new Vector2Int(6, 6);
     void Start()
     {
-        
+        int numCells = GridDimensions.x * GridDimensions.y;
+
+        while (transform.childCount < numCells)
+        {
+            GameObject newObject = Instantiate(ResourceSlotPrefab, this.transform);
+        }
+        Debug.Log("Generated Item Slots");
     }
 
-    void Update()
-    {
-        
-    }
 }
